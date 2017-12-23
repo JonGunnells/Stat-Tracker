5ab9abd163a28d6cc3722ae79c29a04b67b8c56f
Jonathan <jonathan.gunnells@yahoo.com>
2017-12-22 23:49:22 -0800

    Began hashmap integration

--- app/src/main/java/com/gunnells/android/stattrackerjj/MainActivity.java
+++ app/src/main/java/com/gunnells/android/stattrackerjj/MainActivity.java
@@ -1,16 +1,19 @@
 package com.gunnells.android.stattrackerjj;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.TextView;
 
+import java.util.HashMap;
+
 public class MainActivity extends Activity {
 
-    int tscore;
-    int jscore;
+    int tScore;
+    int jScore;
+
+    HashMap<Integer, String> hmap = new HashMap<>();
 
 
     @Override
@@ -20,30 +23,37 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
-    public void jscoreUp(View view){
-        displayJ(jscore = jscore +1);
+    public void jScoreUp(View view){
+        jScore = jScore +1;
+        hmap.put(jScore, "Jon");
+        display(jScore);
     }
 
-    public void jscoreDown(View view){
-        displayJ(jscore = jscore -1);
+    public void jScoreDown(View view){
+        jScore = jScore -1;
+        hmap.put(jScore, "Jon");
+        display(jScore);
     }
 
-    public void tscoreUp(View view){
-        display(tscore = tscore +1);
+    public void tScoreUp(View view){
+        tScore = tScore +1;
+        hmap.put(tScore, "Thomas");
+        display(tScore);
     }
 
-    public void tscoreDown(View view) {
-        display(tscore = tscore -1);
-
+    public void tScoreDown(View view) {
+        tScore = tScore -1;
+        hmap.put(tScore, "Thomas");
+        display(tScore);
     }
 
-    public void display(int number){
+    public void display(int tScore){
         TextView thomasScore = findViewById(R.id.thomasScore);
-        thomasScore.setText("" + number);
+        thomasScore.setText("" + tScore);
     }
 
-    public void displayJ(int number){
+    public void displayJ(int jScore){
         TextView jonScore =  findViewById(R.id.jonScore);
-        jonScore.setText("" + number);
+        jonScore.setText("" + jScore);
     }
 }
